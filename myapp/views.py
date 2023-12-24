0acc34bf981889aa4ecdc58f55cc3c7d8e015517
Cristian Abel <cristtian.abel@gmail.com>
2023-12-23 22:30:43 -0400

    se modifico las vista

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from .models import Project,Tareas
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Index page</h1>")
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    projects = list(Project.objects.values()) 
+    return render(request, 'projects.html')
+def taks(request):
+    #tareas = Tareas.objects.get(id=id)
+    #tareas = get_object_or_404(Tareas, id=id)
+    return render(request, 'taks.html')
+
+
+
 def hello(request, usuario):
     print(usuario)
     return HttpResponse("<h1>Hello %s</h1> "% usuario) # uso de parametros: puede ser str, int, etc
-def about(request):
-    return HttpResponse("<h3>mas de nosotros.!</h3>")
 
+'''
 # seccion para hacer  consultas.
 def projects(request):
     projects = list(Project.objects.values()) 
@@ -19,3 +31,4 @@ def taks(request, id):
     #tareas = Tareas.objects.get(id=id)
     tareas = get_object_or_404(Tareas, id=id)
     return HttpResponse('tarea: %s'% tareas.titulo)
+'''
